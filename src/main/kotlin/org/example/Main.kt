fc4cef7185059d5f437febc1e64a9b1bf9e45003
Jeppe Feld <jafeld@gmail.com>
2026-05-05 20:55:45 +0200

    feat: Add topology loader to fetch from database, add run config

--- src/main/kotlin/org/example/Main.kt
+++ src/main/kotlin/org/example/Main.kt
@@ -1,6 +1,7 @@
 package org.example
 
 import org.example.database.Database
+import org.example.database.TopologyLoader
 import org.example.database.createTables
 import org.example.database.seedDemoData
 
@@ -10,5 +11,9 @@ fun main() {
     Database.connect().use { connection ->
         createTables(connection)
         seedDemoData(connection)
+
+        val topologyLoader = TopologyLoader(connection)
+        val nodes = topologyLoader.loadNodes()
+        val links = topologyLoader.loadLinks(nodes)
     }
 }
---
+++ src/main/kotlin/org/example/database/TopologyLoader.kt
@@ -0,0 +1,53 @@
+package org.example.database
+
+import org.example.domain.Link
+import org.example.domain.Node
+import java.sql.Connection
+
+class TopologyLoader(
+    private val connection: Connection
+) {
+    fun loadNodes(): Set<Node> {
+        val nodes = mutableSetOf<Node>()
+
+        connection.createStatement().use { statement ->
+            statement.executeQuery(
+                """
+                SELECT id
+                FROM nodes
+                ORDER BY id
+                """.trimIndent()
+            ).use { resultSet ->
+                while (resultSet.next()) {
+                    nodes.add(Node(resultSet.getString("id")))
+                }
+            }
+        }
+
+        return nodes
+    }
+
+    fun loadLinks(nodes: Set<Node>): Set<Link> {
+        val nodesById = nodes.associateBy { it.id }
+        val links = mutableSetOf<Link>()
+
+        connection.createStatement().use { statement ->
+            statement.executeQuery(
+                """
+                SELECT source_node_id, target_node_id
+                FROM links
+                ORDER BY id
+                """.trimIndent()
+            ).use { resultSet ->
+                while (resultSet.next()) {
+                    val source = nodesById.getValue(resultSet.getString("source_node_id"))
+                    val target = nodesById.getValue(resultSet.getString("target_node_id"))
+
+                    links.add(Link(source, target))
+                }
+            }
+        }
+
+        return links
+    }
+}
